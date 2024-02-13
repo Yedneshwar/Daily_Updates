832972625788090954d8856fc3f953ad17b700cc
Yedneshwar Padmnabah Pinge <84652858+Yedneshwar@users.noreply.github.com>
2024-02-13 10:38:26 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+class OddEven
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        if (a % 2 == 0)
+        
+            System.out.println("The entered number is an even number.\n");
+        
+        else
+        
+            System.out.println("The entered number is an odd number.\n");
+        
+    }
+}
