176ffa48bbbcab53f84f7fb0cf2dff1a23c9886e
Yedneshwar Padmnabah Pinge <84652858+Yedneshwar@users.noreply.github.com>
2024-02-14 13:10:37 +0530

    Add files via upload

---
+++ Student class.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Student 
+{
+    int id;
+    String name;
+    public static void main(String []args)
+    {
+        Student s1 = new Student();
+        System.out.println(s1.id, + , s1.name);
+    }
+    return;
+}
