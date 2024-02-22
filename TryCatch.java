d87ad15cd89fd0a8cf0aab3200b4dcd571808850
Yedneshwar Padmnabah Pinge <yppinge@gmail.com>
2024-02-22 12:41:59 +0530

    Unchecked Exception

---
+++ TryCatch.java
@@ -0,0 +1,15 @@
+public class TryCatch
+{
+public static void main(String []args )
+{
+try {
+int arr[] = {1,2,3,4,5};
+System.out.println(arr[10]);
+}
+catch(ArrayIndexOutOfBoundsException e)
+{
+System.out.println(e);
+}
+System.out.println("Rest of the code runs smooth");
+}
+}
